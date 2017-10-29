103db60e2b0bf8d8b735a810930e5551699a4250
JessikaSmith <mariyaxod@yandex.ru>
2017-10-29 13:09:00 +0300

    step change

--- Definite_integral_calculation/serial.cpp
+++ Definite_integral_calculation/serial.cpp
@@ -3,6 +3,8 @@
 #include <omp.h>
 #include <math.h> 
 #include <chrono>
+#include <fstream>
+#include <iomanip>
 
 using namespace std;
 
@@ -20,12 +22,14 @@ void serial_time(double a, double b, double eps, float step, int num_of_time_sam
 	double edges = (f(a) + f(b)) / 2;
 	double* time = new double[num_of_time_samples];
 	double J_2;
+	ofstream fout;
+	fout.open("short2.txt", ios_base::app);
 	for (int t = 1; t < num_of_time_samples + 1; t++) {
 		J_2 = 10000000;
 
 		std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
 
-		for (int n = 3000; n < 10000000000; n += step) {
+		for (int n = 1; n < 10000000000; n += step) {
 			dd = (b - a) / n;
 			J_1 = J_2;
 			J_2 = 0;
@@ -35,14 +39,14 @@ void serial_time(double a, double b, double eps, float step, int num_of_time_sam
 			}
 			J_2 += edges;
 			J_2 = dd*J_2;
-			//cout << log10(abs(J_1 - J_2)) << endl;
-			//cout << n << endl;
-			if (log10(abs(J_1 - J_2)) <= eps) break;
+			fout << n << ","<< (J_1 - J_2) / J_2 << ","<< J_2 << endl;
+			if (log10(abs((J_1 - J_2)/J_2)) <= eps) break;
 		}
 
 		std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 		time[t] = std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count();
 	}
+	fout.close();
 	double minim = time[0];
 	for (int y = 1; y < num_of_time_samples; y++) {
 		if (time[y] < minim) minim = time[y];
@@ -62,8 +66,6 @@ void parallel_time(double a, double b, double eps, float step, int num_of_thread
 	for (int t = 1; t < num_of_time_samples + 1; t++) {
 		J_2 = 10000000;
 
-		omp_lock_t lock;
-		omp_init_lock(&lock);
 
 		t_1 = omp_get_wtime();
 		dd = 0;
@@ -77,24 +79,20 @@ void parallel_time(double a, double b, double eps, float step, int num_of_thread
 			for (i = 1; i < n - 1; i++) {
 				#pragma omp critical
 				x_i = a + dd*i;
-				omp_set_lock(&lock);
 				J_2 += f(x_i);
-				omp_unset_lock(&lock);
-
 			}
 			J_2 += edges;
 			#pragma omp critical
 			J_2 = dd*J_2;
 			if (log10(abs(J_1 - J_2)) <= eps) break;
 		}
-
 		t_2 = omp_get_wtime();
 		dt = t_2 - t_1;
 		time[t] = dt;
 	}
 
 	double minim = time[0];
-	for (int y = 1; y < num_of_time_samples; y++) {
+	for (int y = 201000; y < num_of_time_samples; y++) {
 		if (time[y] < minim) minim = time[y];
 	}
 	cout << "Result for " << num_of_threads << " threads: " << J_2 << " | Computational time: " << minim << endl;
@@ -107,9 +105,8 @@ int main() {
 	const int num_of_elem = 7;
 	double a[num_of_elem] = { 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10 };
 	double b[num_of_elem] = { 0.0001, 0.001, 0.01, 0.1, 1, 10, 100 };
-	//double eps[num_of_elem] = { -2.77E-11, 1.09E-10, 2.05E-11, -2.22E-12, 8.67E-11, -6.00E-11, -6.00E-11 };
-	double eps[num_of_elem] = { -11, -10, -11, -12, -11, -11, -11 };
-	float initial_step = 10000;
+	double eps[num_of_elem] = { -9, -10, -11, -12, -11, -11, -11 };
+	float initial_step = 10;
 	int num_of_time_samples = 1;
 
 	for (int i = 0; i < num_of_elem; i++) {
