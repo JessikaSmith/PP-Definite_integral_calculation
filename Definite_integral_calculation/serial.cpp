9a08ec51851f753564e6aafbdc0dd75e2fa0ad85
JessikaSmith <mariyaxod@yandex.ru>
2017-11-11 13:26:58 +0300

    Error in parallel computations fixed

--- Definite_integral_calculation/serial.cpp
+++ Definite_integral_calculation/serial.cpp
@@ -3,27 +3,26 @@
 #include <omp.h>
 #include <math.h> 
 #include <chrono>
-//#include <fstream>
-//#include <iomanip>
 
 using namespace std;
 
 static long npoints = 100;
 
+
 double f(float x) {
 	return 1 / pow(x,2) * pow(sin(1 / x),2);
 }
 
-void serial_time(double a, double b, double eps, float step, int num_of_time_samples = 1) {
+void serial_time(double a, double b, double eps, float step) {
 
 	double x_i, dd, J_1, t_1, t_2, dt;
 	double edges = (f(a) + f(b)) / 2;
 	//double* time = new double[num_of_time_samples];
 	double J_2 = 10000000;
 
-	chrono::high_resolution_clock::time_point begin = chrono::high_resolution_clock::now();
+	chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
 
-	for (int n = 1; n < 10000000000; n *= step) {
+	for (int n = 1; n < 10000000000; n > 100000000 ? n += 10 : n *= step ) {
 		dd = (b - a) / n;
 		J_1 = J_2;
 		J_2 = 0;
@@ -33,6 +32,7 @@ void serial_time(double a, double b, double eps, float step, int num_of_time_sam
 		}
 		J_2 += edges;
 		J_2 = dd*J_2;
+		cout << log10(abs((J_1 - J_2) / J_2)) <<" "<<J_2 << endl;
 		if (log10(abs((J_1 - J_2)/J_2)) <= eps){
 			cout << "Result for serial program: " << J_2;
 			break;
@@ -40,37 +40,34 @@ void serial_time(double a, double b, double eps, float step, int num_of_time_sam
 	}
 
 	chrono::high_resolution_clock::time_point end = chrono::high_resolution_clock::now();
-	auto duration = chrono::duration_cast<chrono::seconds>(end_time - start_time).count();
+	auto duration = chrono::duration_cast<chrono::seconds>(end - start).count();
 
 	cout << " | Computational time: " << duration << endl;
-
 }
+
 
 void parallel_time(double a, double b, double eps, float step, int num_of_threads, int num_of_time_samples = 1) {
 
 	double x_i, dd, J_1, t_1, t_2, dt;
 	double edges = (f(a) + f(b)) / 2;
 	double J_2 = 10000000;
-	int n, i;
+	int i;
 
 	t_1 = omp_get_wtime();
-	dd = 0;
 
-	for (n = 1; n < 10000000000; n *= step) {
-		dd += (b - a) / n;
+	for (int n = 1; n < 10000000000; n > 100000000 ? n += 10 : n *= step) {
+		dd = (b - a) / n;
 		J_1 = J_2;
 		J_2 = 0;
 		#pragma omp parallel for schedule(static) private(i) \
-			num_threads(num_of_threads) reduction(+:f(x_i))
+			num_threads(num_of_threads) 
 		for (i = 1; i < n - 1; i++) {
-			#pragma omp critical
-			{
 				x_i = a + dd*i;
 				J_2 += f(x_i);
-			}
 		}
 		J_2 += edges;
 		J_2 = dd*J_2;
+		cout << log10(abs((J_1 - J_2) / J_2)) <<" "<< J_2 << endl;
 		if (log10(abs((J_1 - J_2) / J_2)) <= eps) {
 			cout << "Result for " << num_of_threads << " threads: " << J_2;
 			break;
@@ -80,9 +77,7 @@ void parallel_time(double a, double b, double eps, float step, int num_of_thread
 	dt = t_2 - t_1;
 
 	cout << " | Computational time: " << dt << endl;
-
 }
-
 
 int main() {
 
@@ -90,12 +85,12 @@ int main() {
 	double a[num_of_elem] = { 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10 };
 	double b[num_of_elem] = { 0.0001, 0.001, 0.01, 0.1, 1, 10, 100 };
 	double eps[num_of_elem] = { -9, -10, -11, -12, -11, -11, -11 };
-	float initial_step = ;
+	float initial_step = 2;
 	int num_of_time_samples = 1;
 
 	for (int i = 0; i < num_of_elem; i++) {
 		cout << "===========  a = " << a[i] << ", b = " << b[i] << " ===========" << endl;
-		serial_time(a[i], b[i], eps[i], initial_step);
+		// serial_time(a[i], b[i], eps[i], initial_step);
 		for (int num_of_threads = 2; num_of_threads < 17; num_of_threads += 2) {
 			parallel_time(a[i], b[i], eps[i], initial_step, num_of_threads);
 		}
