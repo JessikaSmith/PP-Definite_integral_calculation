35c99ce9c10e676249f710a505cf912f3c6f8f54
JessikaSmith <mariyaxod@yandex.ru>
2017-11-13 00:35:52 +0300

    w

--- Definite_integral_calculation/serial.cpp
+++ Definite_integral_calculation/serial.cpp
@@ -32,9 +32,9 @@ void serial_time(double a, double b, double eps, float step) {
 		}
 		J_2 += edges;
 		J_2 = dd*J_2;
-		// cout << log10(abs((J_1 - J_2) / J_2)) <<" "<<J_2 << endl;
+		//cout << log10(abs((J_1 - J_2) / J_2)) <<" "<<J_2<< " and n = " << n << " points." << endl;
 		if (log10(abs((J_1 - J_2)/J_2)) <= eps){
-			cout << "Result for serial program: " << J_2 << " and n = " << n << " points.";;
+			cout << "Result for serial program: " << J_2 << " and n = " << n << " points.";
 			break;
 		}
 	}
@@ -59,11 +59,16 @@ void parallel_time(double a, double b, double eps, float step, int num_of_thread
 		dd = (b - a) / n;
 		J_1 = J_2;
 		J_2 = 0;
-		#pragma omp parallel for schedule(static) private(i, x_i) shared(a, dd, n) reduction(+:J_2) num_threads(num_of_threads)
+		omp_lock_t lock;
+		omp_init_lock(&lock);
+		#pragma omp parallel for schedule(static) private(x_i) shared(a, dd, n) num_threads(num_of_threads)
 		for (i = 1; i < n - 1; i++) {
 				x_i = a + dd*i;
+				omp_set_lock(&lock);
 				J_2 += f(x_i);
+				omp_unset_lock(&lock);
 		}
+		omp_destroy_lock(&lock);
 		J_2 += edges;
 		J_2 = dd*J_2;
 		// cout << log10(abs((J_1 - J_2) / J_2)) <<" "<< J_2 << endl;
@@ -86,13 +91,10 @@ int main() {
 	double eps[num_of_elem] = { -8, -10, -11, -11, -11, -11, -11 };
 	float initial_step = 2;
 
-	for (int i = 0; i < num_of_elem; i++) {
+	// parallel_time(a[4], b[4], eps[4], initial_step, 2);
+	for (int i = 6; i > 0; i--) {
 		cout << "===========  a = " << a[i] << ", b = " << b[i] << " ===========" << endl;
-		// 0 - 797
 		serial_time(a[i], b[i], eps[i], initial_step);
-		//for (int num_of_threads = 2; num_of_threads < 17; num_of_threads += 2) {
-		//	parallel_time(a[i], b[i], eps[i], initial_step, num_of_threads);
-		//}
 		cout << endl;
 	}
 }
