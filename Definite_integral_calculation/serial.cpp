d94d8915c9e115692db4f3ac58211b9f6b418424
JessikaSmith <mariyaxod@yandex.ru>
2017-11-12 14:06:09 +0300

    decreasing eps

--- Definite_integral_calculation/serial.cpp
+++ Definite_integral_calculation/serial.cpp
@@ -22,7 +22,7 @@ void serial_time(double a, double b, double eps, float step) {
 
 	chrono::high_resolution_clock::time_point start = chrono::high_resolution_clock::now();
 
-	for (int n = 1; n < 10000000000; n > 100000000 ? n += 10 : n *= step ) {
+	for (int n = 1; n < 10000000000; n > 100000000 ? n += 10000 : n *= step ) {
 		dd = (b - a) / n;
 		J_1 = J_2;
 		J_2 = 0;
@@ -32,7 +32,7 @@ void serial_time(double a, double b, double eps, float step) {
 		}
 		J_2 += edges;
 		J_2 = dd*J_2;
-		cout << log10(abs((J_1 - J_2) / J_2)) <<" "<<J_2 << endl;
+		// cout << log10(abs((J_1 - J_2) / J_2)) <<" "<<J_2 << endl;
 		if (log10(abs((J_1 - J_2)/J_2)) <= eps){
 			cout << "Result for serial program: " << J_2;
 			break;
@@ -55,7 +55,7 @@ void parallel_time(double a, double b, double eps, float step, int num_of_thread
 
 	t_1 = omp_get_wtime();
 
-	for (int n = 1; n < 10000000000; n > 100000000 ? n += 10 : n *= step) {
+	for (int n = 1; n < 10000000000; n > 100000000 ? n += 10000 : n *= step) {
 		dd = (b - a) / n;
 		J_1 = J_2;
 		J_2 = 0;
@@ -67,7 +67,7 @@ void parallel_time(double a, double b, double eps, float step, int num_of_thread
 		}
 		J_2 += edges;
 		J_2 = dd*J_2;
-		cout << log10(abs((J_1 - J_2) / J_2)) <<" "<< J_2 << endl;
+		// cout << log10(abs((J_1 - J_2) / J_2)) <<" "<< J_2 << endl;
 		if (log10(abs((J_1 - J_2) / J_2)) <= eps) {
 			cout << "Result for " << num_of_threads << " threads: " << J_2;
 			break;
@@ -84,12 +84,13 @@ int main() {
 	const int num_of_elem = 7;
 	double a[num_of_elem] = { 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10 };
 	double b[num_of_elem] = { 0.0001, 0.001, 0.01, 0.1, 1, 10, 100 };
-	double eps[num_of_elem] = { -9, -10, -11, -12, -11, -11, -11 };
+	double eps[num_of_elem] = { -8, -10, -11, -12, -11, -11, -11 };
 	float initial_step = 2;
 	int num_of_time_samples = 1;
 
 	for (int i = 0; i < num_of_elem; i++) {
 		cout << "===========  a = " << a[i] << ", b = " << b[i] << " ===========" << endl;
+		// 0 - 797
 		// serial_time(a[i], b[i], eps[i], initial_step);
 		for (int num_of_threads = 2; num_of_threads < 17; num_of_threads += 2) {
 			parallel_time(a[i], b[i], eps[i], initial_step, num_of_threads);
